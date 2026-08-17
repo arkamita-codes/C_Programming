91859fda97c4f657cceb8c2ea3a24b8714d0f04d
arkamita-codes <arkamitaboxi42@gmail.com>
2026-08-18 00:21:04 +0530

    assignment_16.c

--- Day_02/assignment_16.c
+++ Day_02/assignment_16.c
@@ -1,11 +1,14 @@
+//WACP tp calculate the sum of all digits of a three digit number.
 #include <stdio.h>
 int main()
 {
     int sum = 0;
     int n; 
     int d, copy = 0;
+    printf("-----------------------------------\n");
     printf("Enter a three digit number: ");
     scanf("%d",&n);
+    printf("-----------------------------------\n");
     copy = n;
 
     d = n%10;
@@ -23,6 +26,7 @@ int main()
     n /= 10;
 
     printf("The sum of the digits of a three digit number %d is %d", copy,sum);
+    printf("\n-----------------------------------\n");
 
     return 0;
 }
