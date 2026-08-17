265c929b1b60b0302fd8aec3e0b321de90ff1229
arkamita-codes <arkamitaboxi42@gmail.com>
2026-08-18 00:18:53 +0530

    assignment_13.c

--- Day_02/assignment_13.c
+++ Day_02/assignment_13.c
@@ -1,15 +1,19 @@
+//WCAP to find the square root of a number.
 #include <stdio.h>
 #include <math.h>
 int main()
 {
     int a;
     float sq_root = 0.0;
+    printf("-----------------------------------\n");
     printf("Enter a number: ");
     scanf("%d",&a);
+    printf("-----------------------------------\n");
     
     sq_root = sqrt(a);
 
     printf("\nThe square root of %d is: %.3f\n",a,sq_root);
+    printf("-----------------------------------\n");
     
     return 0;
 
