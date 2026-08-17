4a62b21048cbb4dca58432da00a39b2ec0590e4e
arkamita-codes <arkamitaboxi42@gmail.com>
2026-08-17 23:58:08 +0530

    assignment_12.c

--- Day_02/assignment_12.c
+++ Day_02/assignment_12.c
@@ -1,17 +1,22 @@
+//WCAP to swap to two numbers without using third variable.
 #include <stdio.h>
 int main()
 {
     int a,b;
+    printf("-----------------------------------\n");
     printf("Enter a number: ");
     scanf("%d", &a);
     printf("Enter a number: ");
     scanf("%d", &b);
+    printf("-----------------------------------\n");
+
 
     a = a + b;
     b = a - b;
     a = a - b;
 
-    printf("After swapping\n a = %d \n b = %d", a,b);
+    printf("After swapping\na = %d \nb = %d\n", a,b);
+    printf("-----------------------------------\n");
 
     return 0;
 }
