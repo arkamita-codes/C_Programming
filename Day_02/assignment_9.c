6e194e110b8deda77b66aa47168c42feadc9b252
arkamita-codes <arkamitaboxi42@gmail.com>
2026-08-17 23:56:30 +0530

    assignment_9.c

--- Day_02/assignment_9.c
+++ Day_02/assignment_9.c
@@ -1,10 +1,14 @@
+//WCAP to find the ASCII value of a character.
 #include <stdio.h>
 int main()
 {
     char c;
+    printf("-----------------------------------\n");
     printf("Enter a character: ");
     scanf("%c",&c);
+    printf("-----------------------------------\n");
     printf("The ASCII value of %c is %d",c,c);
+    printf("\n-----------------------------------\n");
 
     return 0;
 }
