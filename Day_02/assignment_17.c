36cee49c2fde63bd9f28f97564c4fbacec8c47f7
arkamita-codes <arkamitaboxi42@gmail.com>
2026-08-18 00:22:20 +0530

    assignment_17.c

--- Day_02/assignment_17.c
+++ Day_02/assignment_17.c
@@ -1,3 +1,4 @@
+//WACP to find out the squares of sum of first 10 natural numbers without using loop
 #include <stdio.h>
 int main()
 {
@@ -5,7 +6,9 @@ int main()
     sum = (1+2+3+4+5+6+7+8+9+10);
     sq_sum = sum * sum;
 
+    printf("-----------------------------------\n");
     printf("The sum of the squares of first ten natural numbers is %d", sq_sum);
+    printf("\n-----------------------------------\n");
 
     return 0;
 }
