d449338c6ecd5d7d5bd819374e9a27a7d364ec80
arkamita-codes <arkamitaboxi42@gmail.com>
2026-08-18 00:20:23 +0530

    assignment_15.c

--- Day_02/assignment_15.c
+++ Day_02/assignment_15.c
@@ -1,9 +1,11 @@
+//WACP to find out the root of a quadratic equation.
 #include <stdio.h>
 #include <math.h>
 int main()
 {
     int a, b, c;
     float root_1 = 0.0, root_2 = 0.0, discriminant = 0.0;
+    printf("-----------------------------------\n");
     printf("The quadratic equation in the form: \nax2 + bx + c = 0\n\n");
     printf("The coefficient of a:");
     scanf("%d",&a);
@@ -11,13 +13,15 @@ int main()
     scanf("%d",&b);
     printf("The coefficient of c:");
     scanf("%d",&c);
+    printf("-----------------------------------\n");
 
     discriminant = (sqrt((pow(b,2)) - 4*a*c));
     root_1 = (-b + discriminant)/(2*a);
     root_2 = (-b - discriminant)/(2*a);
 
-    printf("\nThe first root of the equation is: %.3f\n",root_1);
+    printf("The first root of the equation is: %.3f\n",root_1);
     printf("The second root of the equation is: %f",root_2);
+    printf("\n-----------------------------------\n");
 
     return 0;
 
